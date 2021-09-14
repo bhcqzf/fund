5f81c15959566ab31628e770e6b2e9378d3ba880
bai4246464@gmail.com <Bai:000123>
2021-09-14 22:19:11 +0800

    添加了一个service

--- SSM/src/main/java/cn/bigbaic/controller/MyController.java
+++ SSM/src/main/java/cn/bigbaic/controller/MyController.java
@@ -1,5 +1,14 @@
 package cn.bigbaic.controller;
 
+import cn.bigbaic.service.SayHello;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
 /**
  * <h3>spirngmvc-study</h3>
  * <p></p>
@@ -8,5 +17,18 @@ package cn.bigbaic.controller;
  * @date : 2021-07-30 11:20
  **/
 
+@Controller
+@RequestMapping(value="/student")
 public class MyController {
+    @Autowired
+    private SayHello sayHello;
+
+    @ResponseBody
+    @RequestMapping(value="/hello",method = {RequestMethod.GET})
+    public String niHao(){
+        sayHello.sayHello();
+        String res = "{hello:nihaoya}";
+        return res;
+    }
+
 }
