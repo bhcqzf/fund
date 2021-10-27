6d744dc0ccba5ab12fa844c1be82ad3396a00722
bai <bai4246464@gmail.com>
2021-10-27 14:43:01 +0800

    初始化程序

--- SSM/src/main/java/cn/bigbaic/controller/MyController.java
+++ SSM/src/main/java/cn/bigbaic/controller/MyController.java
@@ -1,5 +1,8 @@
 package cn.bigbaic.controller;
 
+import cn.bigbaic.dao.FundcodeDao;
+import cn.bigbaic.domain.Fundcode;
+import cn.bigbaic.service.DoParse;
 import cn.bigbaic.service.SayHello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,16 +21,26 @@ import javax.annotation.Resource;
  **/
 
 @Controller
-@RequestMapping(value="/student")
+@RequestMapping
 public class MyController {
     @Autowired
-    private SayHello sayHello;
+    private DoParse doParse;
+    
+    @Autowired
+    private FundcodeDao fundcodeDao;
 
     @ResponseBody
-    @RequestMapping(value="/hello",method = {RequestMethod.GET})
-    public String niHao(){
-        sayHello.sayHello();
-        String res = "{hello:nihaoya}";
+    @RequestMapping(value="/getfund",method = {RequestMethod.GET})
+    public String getfund(){
+        Fundcode[] fundcodes = fundcodeDao.selectFundcode();
+        String res = "";
+        for (Fundcode fundcode:fundcodes) {
+            System.out.println(fundcode);
+            boolean issend = doParse.parse(fundcode.getFundcode());
+            if ( !issend ){
+                res = doParse.sendMsg(fundcode.getFundcode());
+            }
+        }
         return res;
     }
 
